420bcdd50e852c04388f331d91c18eef61799c81
Axell <70349718+AxellGithub1@users.noreply.github.com>
2026-04-08 05:25:38 +0200

    Some testing at 5 am

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
-print("Hello World!")
+string = input("Enter your numbers: ")
+array = string.split()
+numbers = [int(x) for x in array]
+even = 0
+suma = 0
+for i in numbers:
+    suma += i
+    if i % 2 == 0:
+        even += 1
+
+print("Sum: " + str(suma) + "\nMin: " + str(min(numbers)) + "\nMax: " + str(max(numbers)) + "\nEven numbers: " + str(even))
